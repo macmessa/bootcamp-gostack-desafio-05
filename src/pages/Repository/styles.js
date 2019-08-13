a7b45bab47c0fd09da1b9a07ccaf8dd581431d1d
macmessa <mac.messa@gmail.com>
2019-08-12 23:10:47 -0300

    feat: set label text color with polished

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -1,12 +1,5 @@
 import styled, { keyframes } from 'styled-components';
-
-function pickTextColor(bgColor) {
-  const color = bgColor.charAt(0) === '#' ? bgColor.substring(1, 7) : bgColor;
-  const r = parseInt(color.substring(0, 2), 16); // hexToR
-  const g = parseInt(color.substring(2, 4), 16); // hexToG
-  const b = parseInt(color.substring(4, 6), 16); // hexToB
-  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? '#000' : '#fff';
-}
+import { getLuminance } from 'polished';
 
 const load = keyframes`
   0% {
@@ -150,7 +143,8 @@ export const IssueList = styled.ul`
 
 export const IssueLabel = styled.span`
   background: ${props => props.background};
-  color: ${props => pickTextColor(props.background)};
+  color: ${props =>
+    props.background && getLuminance(props.background) < 0.5 ? '#fff' : '#000'};
   border-radius: 2px;
   font-size: 12px;
   font-weight: 600;
